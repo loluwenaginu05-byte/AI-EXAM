a2ba4d800bbbefe79e53001265ada6bff40d7e91
loluwenaginu05-byte <loluwenaginu05@gmail.com>
2026-03-27 17:34:09 +0800

    完成代码类相关创建

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import random
+import time
+
+class Student:
+    #创建数据类，用来储存学生的相关信息
+    def __init__(self, serial_number, name, gender, class_name, student_id, college):#序号	姓名	性别	班级	学号	学院
+        # 初始化学生属性
+        self.serial_number=serial_number
+        self.name = name
+        self.gender = gender
+        self.class_name = class_name
+        self.student_id = student_id
+        self.college = college
+
+    def __str__(self):
+        # 定义函数直接打印学生信息
+        return f"学号{self.serial_number},姓名：{self.name}, 性别：{self.gender}, 班级：{self.class_name}, 学号：{self.student_id}, 学院：{self.college}"
+        pass
+
+class ExamSystem:
+    #创建考场系统
+    def __init__(self, data_file):#data_file=“.txt文件名”
+        self.students = []
+        self.data_file = data_file
+        # 启动时自动加载数据
+        self.load_data()
+
+    @staticmethod
+    def validate_student_id(student_id):
+        return student_id.isdigit()
